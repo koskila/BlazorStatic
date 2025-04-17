4f78e8a853485a444670843860a80e82b7e4b230
Melty Observer <164113402+MeltyObserver@users.noreply.github.com>
2025-04-17 18:32:24 +0000

    Add support for multiple routes in a single page (#59)
    * add support for multiple routes in a single page
    
    * add comments to the RoutesHelper class
    
    * added a demo for multiple routes support
    
    * use '@attribute [Route(...)]' in the MultiRoutes demo
    
    * demo style, version for nuget
    
    ---------
    
    Co-authored-by: tesar-tech <jan@tesar.tech>

--- src/Services/RoutesHelper.cs
+++ src/Services/RoutesHelper.cs
@@ -1,47 +1,51 @@
-using Microsoft.AspNetCore.Components;
 using System.Reflection;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorStatic.Services;
 
-// borrowed code with minor changes
-// https://andrewlock.net/finding-all-routable-components-in-a-webassembly-app/
+/// <summary>
+/// RoutesHelper is responsible for extracting static, non-parameterized routes from Blazor components within a given assembly. <br />
+/// code is borrowed with some changes from:
+/// https://andrewlock.net/finding-all-routable-components-in-a-webassembly-app/
+/// </summary>
 internal static class RoutesHelper
 {
     /// <summary>
     ///     Gets the static routes of a blazor app
     /// </summary>
     /// <param name="assembly">assembly of the blazor app</param>
-    /// <returns>a list of static routes</returns>
-    public static List<string> GetRoutesToRender(Assembly assembly)
+    /// <returns>An array of static routes</returns>
+    public static string[] GetRoutesToRender(Assembly assembly)
     {
-        // Get all the components whose base class is ComponentBase
-        var components = assembly
-            .ExportedTypes
-            .Where(t => t.IsSubclassOf(typeof(ComponentBase)));
-
-        // get all the routes that don't contain parameters
-        List<string> routes = components
-            .Select(GetRouteFromComponent)
-            .Where(route => route is not null)
-            .ToList()!;//previous null check guarantee not nulls 
-
-        return routes;
+        return assembly.ExportedTypes
+            .Where(t => t.IsSubclassOf(typeof(ComponentBase)))
+            .SelectMany(GetRoutesFromComponent)
+            .OfType<string>()
+            .ToArray();
     }
 
     /// <summary>
-    ///     Gets the route from a blazor component
+    ///     Gets the static routes of a blazor component
     /// </summary>
     /// <param name="component"></param>
     /// <returns>
-    ///     The route of the component.
-    ///     Returns null if the component is not a page (doesn't have RouteAttr) or the route has parameters.
+    ///     An array of static routes. <br />
+    ///     Array values can contain null. <br />
+    ///     Returns an empty array if the component doesn't have `@page` directive.
     /// </returns>
-    private static string? GetRouteFromComponent(Type component)
+    private static string[] GetRoutesFromComponent(Type component)
     {
-        var attributes = component.GetCustomAttributes(true);
-
-        // can't work with parameterized pages (such pages has params defined with {paramName})
-        return attributes.OfType<RouteAttribute>()
-            .FirstOrDefault(x => !x.Template.Contains('{'))?.Template;
+        var attributes = component.GetCustomAttributes(typeof(RouteAttribute), inherit: false);
+        var routes = new string[attributes.Length];
+        for(int i = 0; i < attributes.Length; i++)
+        {
+            var attr = (RouteAttribute)attributes[i];
+            // Ignore parameterized routes (e.g /{Id}) because we can't generate them.
+            if(!attr.Template.Contains('{'))
+            {
+                routes[i] = attr.Template;
+            }
+        }
+        return routes;
     }
 }
